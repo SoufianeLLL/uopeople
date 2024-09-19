eb5fd47b0475c744584a2dcc6343863d6caf6904
Soufiane L <aimax.frix@gmail.com>
2024-09-20 00:27:33 +0100

    Add files via upload

---
+++ EcommerceSystem/src/com/ecommerce/Product.java
@@ -0,0 +1,45 @@
+package com.ecommerce;
+
+public class Product {
+    private int productID;
+    private String name;
+    private double price;
+
+    // Constructor
+    public Product(int productID, String name, double price) {
+        this.productID = productID;
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getters and Setters
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Display product details
+    @Override
+    public String toString() {
+        return "Product Id: " + productID + ", Name: " + name + ", Price: " + price;
+    }
+}
