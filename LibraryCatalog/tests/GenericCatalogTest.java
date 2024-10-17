579c17838bc55dbd60b8058893cc23add054b5ed
Soufiane L <aimax.frix@gmail.com>
2024-10-18 00:30:02 +0100

    Create GenericCatalogTest.java

---
+++ LibraryCatalog/tests/GenericCatalogTest.java
@@ -0,0 +1,23 @@
+
+public class GenericCatalogTest {
+
+    public static void main(String[] args) {
+
+        GenericCatalog<LibraryItem> testCatalog = new GenericCatalog<>();
+        
+        // Test adding items
+        testCatalog.addItem(new LibraryItem<>("Harry Potter and the Goblet of Fire", "J. K. Rowling", 1));
+        testCatalog.addItem(new LibraryItem<>("DVD 2006 FIFA World Cup", "Circle Studio Limited", 2));
+        testCatalog.addItem(new LibraryItem<>("The Book of Bill", "Alex Hirsch", 3));
+        
+        // Test viewing catalog
+        testCatalog.printCatalog();
+        
+        // Test removing item
+        testCatalog.removeItem(1);
+        testCatalog.printCatalog();
+        
+        // Test removing non existent item
+        testCatalog.removeItem(99);
+    }
+}
